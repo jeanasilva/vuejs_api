75c9950559481126bd0fbec5228759cb1a631c91
Jean DaSilva/LGESP IT CNS Team - MAO(jean.dasilva@lgepartner.com) <jean.dasilva@lge.com>
2022-07-14 16:00:58 -0400

    Update Project included API Service and ref GetAll

--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,3 +1,36 @@
 <template>
     <h1>Lista de Tarefas</h1>
+    <ul>
+        <li v-for="todo in todos" :key="todo.identifify">
+            {{ todo.title }}
+            {{ todo.body }}
+            {{ todo.completed }}
+        </li>
+    </ul>
+    <input type="text" v-model="name">
 </template>
+
+<script>
+
+import { onMounted, ref } from 'vue'
+import TodoService from '@/services/todos.services'
+
+export default {
+
+    name: 'Todos',
+    setup() {
+
+        const todos = ref([])
+
+        onMounted(() => { 
+            TodoService.getAll().then(response => {
+                todos.value = response.data.data}).catch(error => console.log(error))
+        })
+
+        return {
+            todos
+        }
+    }
+}
+
+</script>
